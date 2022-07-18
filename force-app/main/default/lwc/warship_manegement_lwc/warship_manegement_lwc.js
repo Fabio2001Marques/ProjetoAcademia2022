fb15735a56461ea18f2353ff790ed85bf7428326
Fabio2001Marques <fabio2001marques@gmail.com>
2022-07-18 10:23:15 +0100

    Supply Table + Milestone Table with Data

--- force-app/main/default/lwc/warship_manegement_lwc/warship_manegement_lwc.js
+++ force-app/main/default/lwc/warship_manegement_lwc/warship_manegement_lwc.js
@@ -1,13 +1,31 @@
 import { LightningElement , wire, track} from 'lwc';
+import SUPPLYNAME_FIELD from '@salesforce/schema/Warship_Supply__c.Supply_Name__c';
+import SUPPLYQUANTITY_FIELD from '@salesforce/schema/Warship_Supply__c.Quantity__c';
+import MILSTONE_NAME_FIELD from '@salesforce/schema/Milestone__c.Name';
+import MILSTONE_DUEDATE_FIELD from '@salesforce/schema/Milestone__c.Due_Date__c';
+import MILSTONE_STATUS_FIELD from '@salesforce/schema/Milestone__c.Status__c';
 
-import SUPPLYNAME_FIELD from '@salesforce/schema/Warship_Supply__c.Supply__r.Name';
-import SUPPLYQUANTITY_FIELD from '@salesforce/schema/Warship_Supply__c.Supply__r.Quantity__c';
 import getWarships from '@salesforce/apex/lwc_ManegementWarships.getWarships';
 import getSupplies from '@salesforce/apex/lwc_ManegementWarships.getSupllies';
+import getMilestones from '@salesforce/apex/lwc_ManegementWarships.getMilestones';
 
-const COLUMNS = [
+const SUPPLY_COLUMNS = [
     { label: 'Supply Name', fieldName: SUPPLYNAME_FIELD.fieldApiName, type: 'text' },
     { label: 'Supply Quantity', fieldName: SUPPLYQUANTITY_FIELD.fieldApiName, type: 'text' },
+    {type: 'button', typeAttributes: {  
+        label: 'Add Quantity',  
+        name: 'Add Quantity',  
+        title: 'Add Quantity',  
+        disabled: false,  
+        value: 'AddQuantity',  
+        iconPosition: 'left'  
+    }},  
+];
+
+const MILESTONE_COLUMNS = [
+    { label: 'Milestone Name', fieldName: MILSTONE_NAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Due Date', fieldName: MILSTONE_DUEDATE_FIELD.fieldApiName, type: 'date' },
+    { label: 'Status', fieldName: MILSTONE_STATUS_FIELD.fieldApiName, type: 'text' },
 ];
 
 let i=0;
@@ -16,6 +34,10 @@ export default class Warship_manegement_lwc extends LightningElement {
     @track items = []; 
     @track value = ''; 
     @track chosenValue = '';
+    supplyColumns = SUPPLY_COLUMNS;
+    milestoneColumns = MILESTONE_COLUMNS;
+    supplies;
+    milestones;
 
     @wire(getWarships)
     wiredWarships({ error, data }) {
@@ -32,19 +54,34 @@ export default class Warship_manegement_lwc extends LightningElement {
     get warshipOption() {
         return this.items;
     }
-
-    handleChange(event) {
-        const selectedOption = event.detail.value;
-        this.chosenValue = selectedOption;
-    }
-
     get selectedValue(){
         return this.chosenValue;
     }
 
-    columns = COLUMNS;
-    @wire(getSupplies, {wId: '$chosenValue'})
-    supplies;
-
-
+    handleChange(event) {
+        const selectedOption = event.detail.value;
+        this.chosenValue = selectedOption;
+        
+        getSupplies({wId: this.chosenValue})
+            .then((result)=> {
+                this.supplies = result;
+                this.error = undefined;
+            })
+            .catch((error) =>{
+                this.supplies = undefined;
+                this.error = error;
+                console.log(error.body.message);
+            });
+        
+        getMilestones({wId: this.chosenValue})
+        .then((result)=> {
+            this.milestones = result;
+            this.error = undefined;
+        })
+        .catch((error) =>{
+            this.milestones = undefined;
+            this.error = error;
+            console.log(error.body.message);
+        });
+}
 }
